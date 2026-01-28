61978b09828f6360356c101f66227f15b9fe6f9f
rsimpson2 <rsimpson@ventera.com>
2026-01-27 21:44:53 -0500

    add arctic theme

---
+++ lua/plugins/arctic-theme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "rockyzhang24/arctic.nvim",
+    branch = "v2",
+    lazy = false,
+    priority = 1000,
+    dependencies = {
+      "rktjmp/lush.nvim",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "arctic",
+    },
+  },
+}
