59aafa4234e2845e4f6438b649b690cc58a9429e
rsimpson2 <rsimpson2@me.com>
2026-07-22 09:15:17 -0400

    add yazi plugin

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,9 @@
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false, -- recommended
+  ft = "markdown",
+  keys = {
+    { "<leader>mv", "<cmd>Markview toggle<cr>", desc = "Toggle Markview preview" },
+  },
+  opts = {},
+}
